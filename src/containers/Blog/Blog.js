ca745e364d8a3b35d756b969d638b9b4cb4d5b66
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-09-25 21:03:09 +0200

    Fetch data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import baseService from "axios";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    baseService
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(({ data: posts }) => this.setState({ posts }));
+  }
+
+  render() {
+    const posts = this.state.posts.map(post => (
+      <Post title={post.title} id={post.id} />
+    ));
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
