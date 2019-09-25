af0e01b84cd8104b658ef81a72f53652c4e75244
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-09-25 22:06:57 +0200

    Working on GET,POST,DELETE methods.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,24 +8,46 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedID: null
   };
 
   componentDidMount() {
-    baseService
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(({ data: posts }) => this.setState({ posts }));
+    baseService.get("https://jsonplaceholder.typicode.com/posts").then(resp => {
+      const posts = resp.data.slice(0, 4);
+      const data = posts.map(post => {
+        return { ...post, author: "Jovan" };
+      });
+      this.setState({ posts: data });
+    });
   }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedID: id });
+  };
+
+  deleteHandler = () => {
+    baseService
+      .delete(
+        `https://jsonplaceholder.typicode.com/posts/${this.state.selectedID}`
+      )
+      .then(resp => console.log(resp));
+  };
 
   render() {
     const posts = this.state.posts.map(post => (
-      <Post title={post.title} id={post.id} />
+      <Post
+        title={post.title}
+        key={post.id}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)}
+      />
     ));
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedID} delete={this.deleteHandler} />
         </section>
         <section>
           <NewPost />
