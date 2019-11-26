9ac64d554bf052a14ad71916bd62c719796b27ba
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-11-26 07:19:58 +0100

    SEO

--- src/index.js
+++ src/index.js
@@ -7,5 +7,12 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+    ReactDOM.hydrate(<App />, rootElement);
+} else {
+    ReactDOM.render(<App />, rootElement);
+}
 registerServiceWorker();
